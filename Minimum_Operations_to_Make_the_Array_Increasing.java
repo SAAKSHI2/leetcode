f8f9e95cfaf959d632576007428e6fdb6f8681b3
SAAKSHI2 <saakshibhandari27@gmail.com>
2022-01-15 22:03:40 +0530

    28th commit

---
+++ Minimum_Operations_to_Make_the_Array_Increasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int output=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<=nums[i-1])
+            {
+                output+=(nums[i-1]-nums[i]+1);
+                nums[i]=nums[i-1]+1;
+            }
+        }
+        return output;
+    }
+}
