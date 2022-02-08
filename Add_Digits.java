44674dd36b2ad0ba968b4e9cf343d1be7a4a7061
SAAKSHI2 <saakshibhandari27@gmail.com>
2022-02-09 02:33:54 +0530

    72th commit

---
+++ Add_Digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+       int ans=num;
+        while(ans>9)
+        {
+         num=ans;
+         ans=0;
+        while(num>0)
+        {
+            ans+=(num%10);
+            num=num/10;
+        }
+        }
+        return ans;
+    }
+}
