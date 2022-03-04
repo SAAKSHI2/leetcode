0d845c56de012035928f5829bc45bd407068ff50
SAAKSHI2 <saakshibhandari27@gmail.com>
2022-03-04 20:43:18 +0530

    111th commit

---
+++ Champange_Tower.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[][]=new double[query_row+1][query_row+1];
+     dp[0][0]=poured;
+     for(int i=1;i<=query_row;i++)
+     {
+         for(int j=0;j<=i;j++)
+         {
+             if(dp[i-1][j]>1)
+             {
+               dp[i][j]=((dp[i-1][j]-1)/2.0);
+             }
+               if(j>0&&dp[i-1][j-1]>1){
+                 dp[i][j]+=(dp[i-1][j-1]-1)/2.0;
+             }
+         }
+         
+     }
+        if(dp[query_row][query_glass]>1)
+            return 1;
+        return dp[query_row][query_glass];
+    }
+}
